1a0dce92d97c00c85b16b5b7d1a62b8a8d7f821c
erkenraimbaev <erken.orsk.2015@gmail.com>
2023-07-27 19:22:44 +0500

    be the function better

--- funcs.py
+++ funcs.py
@@ -1,10 +1,10 @@
 def function(a, b):
     """Эта принимает два числа, складывает их и возвращает результат"""
     result = a + b
-    return result
+    return print(result)
 
 
 def second_function(a, b):
     """Эта функция принимает два числа, вычитает b из а и возвращает результат"""
     result = a - b
-    return result
+    return print(result)
