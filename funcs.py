b8171abafc220a390f91e977ff516baee494eec6
erkenraimbaev <erken.orsk.2015@gmail.com>
2023-07-27 19:18:51 +0500

    add second function

--- funcs.py
+++ funcs.py
@@ -2,3 +2,9 @@ def function(a, b):
     """Эта принимает два числа, складывает их и возвращает результат"""
     result = a + b
     return result
+
+
+def second_function(a, b):
+    """Эта функция принимает два числа, вычитает b из а и возвращает результат"""
+    result = a - b
+    return result
